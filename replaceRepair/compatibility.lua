d666d3fdba82a67252246f6f351de401c83f33ca
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-08-25 02:43:17 +0200

    Moved more compatibility code into compatibility.lua

--- replaceRepair/compatibility.lua
+++ replaceRepair/compatibility.lua
@@ -1,10 +1,41 @@
 
-return function()
-	local m = replaceRepair_internal
-	if not m then return end
-	
-	LOG("Replace Repair compatibility code. One or more mods are running an outdated and unsupported version of the library.")
-	
-	m.RootGetTargetArea = SelfTarget.GetTargetArea
-	m.OrigTipImage.Fire = Point(2,2)
+local nullfunction = function() end
+
+lmn_replaceRepair = ReplaceRepair
+
+ReplaceRepair.SetRepairSkill = ReplaceRepair.addSkill
+ReplaceRepair.SetPilotRepairSkill = ReplaceRepair.addSkill
+ReplaceRepair.SetMechRepairSkill = ReplaceRepair.addSkill
+ReplaceRepair.getVersion = ReplaceRepair.getVersion
+ReplaceRepair.GetHighestVersion = ReplaceRepair.getVersion
+ReplaceRepair.mostRecent = ReplaceRepair
+ReplaceRepair.init = nullfunction
+ReplaceRepair.load = nullfunction
+ReplaceRepair.internal_init = nullfunction
+
+if replaceRepair_internal then
+	replaceRepair_internal.RootGetTargetArea = SelfTarget.GetTargetArea
+	replaceRepair_internal.OrigTipImage.Fire = Point(2,2)
+end
+
+function ReplaceRepair:ForPilot(sPilotSkill, sWeapon, sPilotTooltip, sIcon)
+	Assert.ModInitializingOrLoading()
+
+	self:SetRepairSkill{
+		Name = sPilotTooltip[1],
+		Description = sPilotTooltip[2],
+		PilotSkill = sPilotSkill,
+		Weapon = sWeapon,
+		Icon = sIcon
+	}
+end
+
+function ReplaceRepair:ForMech(sMech, sWeapon, sIcon)
+	Assert.ModInitializingOrLoading()
+
+	self:SetRepairSkill{
+		MechType = sMech,
+		Weapon = sWeapon,
+		Icon = sIcon
+	}
 end
