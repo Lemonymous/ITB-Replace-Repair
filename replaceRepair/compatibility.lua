f88a255c5210b77ddea9833278264323104a8d70
Lemonymous <10490534+Lemonymous@users.noreply.github.com>
2021-08-25 22:26:28 +0200

    Updated compatibility
    It should now be possible to simply replace the ReplaceRepair folder of a mod with this new version without further edits in the mod itself.

--- replaceRepair/compatibility.lua
+++ replaceRepair/compatibility.lua
@@ -6,27 +6,35 @@ lmn_replaceRepair = ReplaceRepair
 ReplaceRepair.SetRepairSkill = ReplaceRepair.addSkill
 ReplaceRepair.SetPilotRepairSkill = ReplaceRepair.addSkill
 ReplaceRepair.SetMechRepairSkill = ReplaceRepair.addSkill
-ReplaceRepair.getVersion = ReplaceRepair.getVersion
+ReplaceRepair.GetVersion = ReplaceRepair.getVersion
 ReplaceRepair.GetHighestVersion = ReplaceRepair.getVersion
 ReplaceRepair.mostRecent = ReplaceRepair
 ReplaceRepair.init = nullfunction
 ReplaceRepair.load = nullfunction
 ReplaceRepair.internal_init = nullfunction
 
-if replaceRepair_internal then
-	replaceRepair_internal.RootGetTargetArea = SelfTarget.GetTargetArea
-	replaceRepair_internal.OrigTipImage.Fire = Point(2,2)
+if replaceRepair_internal == nil then
+	replaceRepair_internal = {}
+	setmetatable(replaceRepair_internal, { __index = ReplaceRepair })
+
+	replaceRepair_internal.RootGetTargetArea = Skill_Repair.GetTargetArea
+	replaceRepair_internal.RootGetSkillEffect = Skill_Repair.GetSkillEffect
+	replaceRepair_internal.OrigGetTargetArea = Skill_Repair.GetTargetArea
+	replaceRepair_internal.OrigGetSkillEffect = Skill_Repair.GetSkillEffect
+	replaceRepair_internal.OrigTipImage = Skill_Repair.TipImage
+	replaceRepair_internal.OrigName = Weapon_Texts.Skill_Repair_Name
+	replaceRepair_internal.OrigDescription = Weapon_Texts.Skill_Repair_Description
 end
 
 function ReplaceRepair:ForPilot(sPilotSkill, sWeapon, sPilotTooltip, sIcon)
 	Assert.ModInitializingOrLoading()
 
 	self:SetRepairSkill{
-		Name = sPilotTooltip[1],
-		Description = sPilotTooltip[2],
-		PilotSkill = sPilotSkill,
-		Weapon = sWeapon,
-		Icon = sIcon
+		name = sPilotTooltip[1],
+		description = sPilotTooltip[2],
+		pilotSkill = sPilotSkill,
+		weapon = sWeapon,
+		icon = sIcon
 	}
 end
 
@@ -34,8 +42,8 @@ function ReplaceRepair:ForMech(sMech, sWeapon, sIcon)
 	Assert.ModInitializingOrLoading()
 
 	self:SetRepairSkill{
-		MechType = sMech,
-		Weapon = sWeapon,
-		Icon = sIcon
+		mechType = sMech,
+		weapon = sWeapon,
+		icon = sIcon
 	}
 end
